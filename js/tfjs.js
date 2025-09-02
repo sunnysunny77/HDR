3fa83912e92f8be36cb60a62f827bcb7f10d772a
daniel <contractor@lateralaspect.com.au>
2025-09-02 23:43:10 +0800

    Update tfjs.js

--- js/tfjs.js
+++ js/tfjs.js
@@ -58,60 +58,12 @@ clearBtn.addEventListener("click", () => {
 });
 
 const processCanvas = async (canvas) => {
-  const canvasTensor = tf.browser.fromPixels(canvas).toFloat().div(255);
-  const brightness = canvasTensor.mean(2);
-  const mask = brightness.greater(0.1);
-  const coords = await tf.whereAsync(mask);
-
-  if (coords.shape[0] === 0) {
-    coords.dispose();
-    mask.dispose();
-    canvasTensor.dispose();
-    return null;
-  }
-
-  const coordsData = await coords.array();
-  const ys = coordsData.map(([y]) => y);
-  const xs = coordsData.map(([, x]) => x);
-  const minY = Math.min(...ys), maxY = Math.max(...ys);
-  const minX = Math.min(...xs), maxX = Math.max(...xs);
-
-  const cropped = canvasTensor.slice(
-    [minY, minX, 0],
-    [maxY - minY + 1, maxX - minX + 1, 3]
-  );
-
-  const maxSide = Math.max(cropped.shape[0], cropped.shape[1]);
-  const scale = 20 / maxSide;
-  const newH = Math.round(cropped.shape[0] * scale);
-  const newW = Math.round(cropped.shape[1] * scale);
-
-  const resized = tf.image.resizeBilinear(cropped, [newH, newW]);
-  const topPad = Math.floor((28 - newH) / 2);
-  const leftPad = Math.floor((28 - newW) / 2);
-
-  const padded = tf.pad(
-    resized,
-    [[topPad, 28 - newH - topPad], [leftPad, 28 - newW - leftPad], [0, 0]]
-  );
-
-  const grey = padded.mean(2);
-  const input = grey.expandDims(0).expandDims(-1);
-
-  const prediction = model.predict(input);
+  const img = tf.browser.fromPixels(canvas, 1).resizeBilinear([28, 28, 1]).toFloat().div(255.0).expandDims(0);
+  const prediction = model.predict(img);
   const values = await prediction.data();
   const maxIndex = values.indexOf(Math.max(...values));
-
-  input.dispose();
   prediction.dispose();
-  cropped.dispose();
-  resized.dispose();
-  padded.dispose();
-  grey.dispose();
-  coords.dispose();
-  mask.dispose();
-  canvasTensor.dispose();
-
+  img.dispose();
   return maxIndex;
 };
 
